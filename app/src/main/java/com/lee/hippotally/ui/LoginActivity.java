a4bb4daae49c7e2ac98afe8a0a66c01303de23e8
liluo <lastcrazysun@163.com>
2018-10-17 17:19:03 +0800

    更新登录页面

---
+++ app/src/main/java/com/lee/hippotally/ui/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.lee.hippotally.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ */
+public class LoginActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/lee/hippotally/ui/WelcomeActivity.java
+++ app/src/main/java/com/lee/hippotally/ui/WelcomeActivity.java
@@ -1,5 +1,8 @@
 package com.lee.hippotally.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +10,15 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class WelcomeActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(3000);
+            Intent intent=new Intent(this,RegisterActivity.class);
+            startActivity(intent);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
